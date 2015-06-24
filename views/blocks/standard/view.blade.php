b883d6ee11d15213ea9a8297134e0de012d68f9e
Louis Gandelin <lgandelin@web-access.fr>
2015-06-24 11:17:54 +0200

    Fixed a bug with File block

--- views/blocks/standard/view.blade.php
+++ views/blocks/standard/view.blade.php
@@ -1,1 +1,3 @@
-@include(Shortcut::get_theme() . '::partials.blocks.' . $content->viewPath)
+@if (isset($content->viewPath))
+    @include(Shortcut::get_theme() . '::partials.blocks.' . $content->viewPath)
+@endif
