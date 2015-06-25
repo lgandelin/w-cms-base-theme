a92a783311d8481848d3b1061c2f63ff4d6dc474
Louis Gandelin <lgandelin@web-access.fr>
2015-06-25 11:13:47 +0200

    Updated base theme with last modifications on Laravel package

--- views/structure/block.blade.php
+++ views/structure/block.blade.php
@@ -1,5 +1,5 @@
 @if ($block->display)
     <div class="{{ $block->class }} grid-{{ $block->width }} align-{{ $block->alignment }}">
-        @include (Shortcut::get_theme() . '::' . BlockType::getFrontView($block->type), ['content' => $block->content])
+        @include (Shortcut::get_theme() . '::' . $block->type->front_view, ['content' => $block->content])
     </div>
 @endif
