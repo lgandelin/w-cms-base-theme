ba73ae17c964a47382d13c2c0ea9c072093df6f5
Louis Gandelin <lgandelin@web-access.fr>
2015-09-23 16:26:05 +0200

    Added the "is_indexed" field in Page Entity

--- views/includes/header.blade.php
+++ views/includes/header.blade.php
@@ -5,5 +5,6 @@
     {!! HTML::style('css/styles.css') !!}
     @if ($page->meta_description)<meta name="description" content="{{ $page->meta_description }}" />@endif
     @if ($page->meta_keywords)<meta name="keywords" content="{{ $page->meta_keywords }}" />@endif
+    @if (!$page->is_indexed || !env('ROBOTS_INDEXING'))<meta name="robots" content="noindex, nofollow" />@endif
 </head>
 <body>
